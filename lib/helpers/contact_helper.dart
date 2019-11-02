571fcab5e3dd2dd06adb9de92637464330992e9f
dvalerio <dnotms290>
2019-11-02 14:46:56 -0300

    Modelagem para Contato

--- lib/helpers/contact_helper.dart
+++ lib/helpers/contact_helper.dart
@@ -1,10 +1,11 @@
+import 'package:flutter/cupertino.dart';
 import 'package:sqflite/sqflite.dart';
 
-final String idColumn = Column;
-final String nameColumn = Column;
-final String emailColumn = Column;
-final String phoneColumn = Column;
-final String imgColumn = Column;
+final String idColumn = idColumn;
+final String nameColumn = nameColumn;
+final String emailColumn = emailColumn;
+final String phoneColumn = phoneColumn;
+final String imgColumn = imgColumn;
 
 class ContactHelper {
 
@@ -13,5 +14,37 @@ class ContactHelper {
 class Contact {
   int id;
   String name;
-  String
+  String email;
+  String phone;
+  String img;
+
+  //pega o map
+  Contact.fromMap(Map map){
+    id = map[idColumn];
+    name = map[nameColumn];
+    email = map[emailColumn];
+    phone = map[phoneColumn];
+    img = map[imgColumn];
+  }
+
+  Map toMap(){
+    Map<String, dynamic> map = {
+      nameColumn: name,
+      emailColumn: email,
+      phoneColumn: phone,
+      imgColumn: img
+    };
+    if(id != null){
+      map[idColumn] = id;
+    }
+
+    return map;
+  }
+
+  @override
+  String toString() {
+    return "Contato: id: $id, name: $name, email: $email, phone: $phone, img: $img)";
+  }
+
+
 }
