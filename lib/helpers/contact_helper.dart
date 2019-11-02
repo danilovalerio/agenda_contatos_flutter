074fa426f2f919ffb763d8dc27f38a1b107f4200
dvalerio <dnotms290>
2019-11-02 13:55:47 -0300

    Correção dependencia sqflite e não sqlflite

---
+++ lib/helpers/contact_helper.dart
@@ -0,0 +1,17 @@
+import 'package:sqflite/sqflite.dart';
+
+final String idColumn = Column;
+final String nameColumn = Column;
+final String emailColumn = Column;
+final String phoneColumn = Column;
+final String imgColumn = Column;
+
+class ContactHelper {
+
+}
+
+class Contact {
+  int id;
+  String name;
+  String
+}
