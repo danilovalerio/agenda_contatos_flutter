21748829f7762c8ffd23fe7c514ac778442cb0cc
dvalerio <dnotms290>
2019-11-02 13:30:13 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/danilovalerio/agenda_de_contatos_flutter_app/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.danilovalerio.agenda_de_contatos_flutter_app
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
