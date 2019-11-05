faa8b8ae59ccaa4864740fc885817b40f89aa733
Danilo Valério da Silva <danilo_vs@hotmail.com>
2019-11-05 08:04:47 -0300

    contato page

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
+import 'package:agenda_de_contatos_flutter_app/ui/ContactPage.dart';
 import 'package:agenda_de_contatos_flutter_app/ui/Home.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Home(),
+//  home: Home(),
+  home: ContactPage(),
   debugShowCheckedModeBanner: false,
 ));
 
