0de0bfd8513edf41d143a1f3426d897377083dc2
dvalerio <dnotms290>
2019-11-05 15:25:35 -0300

    Imagem do usuário Inputs Nome, e-mail e telefone

--- lib/ui/ContactPage.dart
+++ lib/ui/ContactPage.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:agenda_de_contatos_flutter_app/helpers/contact_helper.dart';
 import 'package:flutter/material.dart';
 
@@ -10,6 +12,11 @@ class ContactPage extends StatefulWidget {
 }
 
 class _ContactPageState extends State<ContactPage> {
+
+
+
+  bool _userEdited = false;
+
   Contact _editedContact;
 
   @override
@@ -36,6 +43,52 @@ class _ContactPageState extends State<ContactPage> {
         onPressed: (){},
         child: Icon(Icons.save),
         backgroundColor: Colors.green,
+      ),
+      body: SingleChildScrollView(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: <Widget>[
+            GestureDetector(
+              child: Container(
+                width: 140,
+                height: 140,
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  image: DecorationImage(
+                      image: _editedContact.img != null ?
+                      FileImage(File(_editedContact.img)) :
+                      AssetImage("images/robot.png")
+                  ),
+                ),
+              ),
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: "Nome"),
+              onChanged: (text){
+                _userEdited = true;
+                setState(() {
+                  _editedContact.name = text;
+                });
+              },
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: "E-mail"),
+              onChanged: (text){
+                _userEdited = true;
+                _editedContact.email = text;
+              },
+              keyboardType: TextInputType.emailAddress,
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: "Telefone"),
+              onChanged: (text){
+                _userEdited = true;
+                _editedContact.phone = text;
+              },
+              keyboardType: TextInputType.phone,
+            ),
+          ],
+        ),
       ),
     );
   }
